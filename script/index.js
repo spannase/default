875e7bf4278aab2b97461249c5be367cdb621d86
spannase <shreya16pannase@gmail.com>
2022-03-19 18:57:36 +0530

    first

---
+++ script/index.js
@@ -0,0 +1,49 @@
+window.addEventListener('scroll', ()=>{
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY>10)
+});
+
+
+//hide and show  
+
+
+const faqs= document.querySelectorAll('.faq');
+
+faqs.forEach(faq=>{
+    faq.addEventListener("click", ()=>{
+        faq.classList.toggle("open");
+
+        //change icon
+
+        const icon = faq.querySelector (".faq__icon i");
+
+        if(icon.className ===  'uil uil-plus'){
+            icon.className = 'uil uil-minus'
+        }else{
+               icon.className= "uil uil-plus"
+        }
+    })
+});
+
+
+//hide and show  nav bar
+
+const menu = document.querySelector(".nav__menu")
+const menuBtn = document.querySelector("#open-menu-btn")
+const closeBtn = document.querySelector("#close-menu-btn")
+
+menuBtn.addEventListener("click", function(){
+
+    menu.style.display= "flex";
+    closeBtn.style.display="inline-block";
+    menuBtn.style.display= "none"
+
+})
+
+const closeNav= ()=>{
+  
+    menu.style.display= "none";
+    closeBtn.style.display="none";
+    menuBtn.style.display= "inline-block"
+}
+
+closeBtn.addEventListener("click", closeNav)
